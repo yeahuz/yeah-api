4435c56110eaba379a3b9193cf9c243355a1e3aa
jcbbb <avaz28082000@gmail.com>
2023-11-25 02:32:03 +0500

    fix: triggers for updated_at column;

--- db/migrations/20231122101049_initial.down.sql
+++ db/migrations/20231122101049_initial.down.sql
@@ -3,5 +3,6 @@ begin;
 drop table if exists users cascade;
 drop table if exists auth_providers cascade;
 drop table if exists accounts cascade;
+drop table if exists otps cascade;
 
 commit;
--- db/migrations/20231122101049_initial.up.sql
+++ db/migrations/20231122101049_initial.up.sql
@@ -4,7 +4,7 @@ create table if not exists users (
   id bigserial primary key,
   phone varchar(15) unique,
   phone_verified boolean default false,
-  name varchar(255)
+  name varchar(255),
   username varchar(255) unique,
   bio varchar(1024),
   website_url varchar(255),
@@ -15,7 +15,7 @@ create table if not exists users (
   profile_url varchar(255),
   verified boolean default false,
   created_at timestamp with time zone default now() not null,
-  updated_at timestamp with time zone on update now()
+  updated_at timestamp with time zone
 );
 
 create table if not exists auth_providers (
@@ -23,7 +23,7 @@ create table if not exists auth_providers (
   logo_url varchar(255),
   active boolean default false,
   created_at timestamp with time zone default now() not null,
-  updated_at timestamp with time zone on update now()
+  updated_at timestamp with time zone
 );
 
 create table if not exists accounts (
@@ -32,7 +32,7 @@ create table if not exists accounts (
   user_id bigint not null,
   provider_account_id varchar(255) not null,
   created_at timestamp with time zone default now() not null,
-  updated_at timestamp with time zone on update now(),
+  updated_at timestamp with time zone,
 
   foreign key (user_id) references users (id) on delete cascade,
   foreign key (provider) references auth_providers (name) on delete cascade
@@ -45,12 +45,34 @@ create table if not exists otps (
   expires_at timestamp with time zone,
   used boolean default false,
   created_at timestamp with time zone default now() not null,
-  updated_at timestamp with time zone on update now()
+  updated_at timestamp with time zone
 );
 
 create index idx_accounts_user_id on accounts(user_id);
 create index idx_accounts_provider_account_id on accounts(provider_account_id);
 create index idx_otps_hash on otps(hash);
 create unique index udx_accounts_provider_account_id_user_id on accounts(provider_account_id, user_id);
+
+create or replace function update_updated_at_column()
+returns trigger as $$
+begin
+NEW.updated_at = now();
+return NEW;
+end;
+$$ language plpgsql;
+
+DO $$
+DECLARE
+    t text;
+BEGIN
+    FOR t IN
+        SELECT table_name FROM information_schema.columns WHERE column_name = 'updated_at'
+    LOOP
+        EXECUTE format('CREATE TRIGGER trigger_update_timestamp
+                    BEFORE UPDATE ON %I
+                    FOR EACH ROW EXECUTE PROCEDURE update_updated_at_column()', t,t);
+    END loop;
+END;
+$$ language 'plpgsql';
 
 commit;
